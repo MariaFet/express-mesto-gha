fad498d4f3da7349b534dc1643173674df749b4f
Maria Fetisova <racusmaria@gmail.com>
2023-07-18 22:20:33 +0300

    feature: добавить контроллеры, роуты и модели

--- controllers/user.js
+++ controllers/user.js
@@ -1,24 +1,20 @@
 const User = require('../models/user');
-const BadRequestError = require('../errors/BadRequestError');
-const ServerError = require('../errors/ServerError');
-const NotFoundError = require('../errors/NotFoundError');
-
 
 module.exports.getAllUsers = (req, res) => {
   User.find({})
   .then(users => res.status(200).send({data: users}))
-  .catch((err) => {throw new ServerError('Произошла ошибка на сервере')});
+  .catch((err) => {return res.status(500).send({message: 'Произошла ошибка на сервере'});});
 };
 
 module.exports.getUser = (req, res) => {
   User.findById(req.params._id)
   .then((user) => {
-    if (!user) {
-      throw new NotFoundError('Пользователь по указанному _id не найден.');
+    if (user) {
+      return res.status(404).send({message: 'Пользователь по указанному _id не найден.'})
     }
     res.status(200).send({data: user});
   })
-  .catch((err) => {throw new ServerError('Произошла ошибка на сервере')});
+  .catch((err) => {return res.status(500).send({message: 'Произошла ошибка на сервере'});});
 };
 
 module.exports.createUser = (req, res) => {
@@ -40,14 +36,16 @@ module.exports.updateUser = (req, res) => {
   User.findByIdAndUpdate(req.user._id, {name, about}, {new: true, runValidators: true})
   .then((user) => {
     if (!user._id) {
-      throw new NotFoundError('Пользователь по указанному _id не найден.');
-    }
-    if (!user.name || !user.about) {
-      throw new BadRequestError('Переданы некорректные данные при обновлении профиля');
+      return res.status(404).send({message: 'Пользователь по указанному _id не найден.'})
     }
     res.status(200).send({data: user});
   })
-  .catch((err) => {throw new ServerError('Произошла ошибка на сервере')});
+  .catch((err) => {
+    if (err.name === 'ValidationError') {
+      return res.status(400).send({message: 'Переданы некорректные данные при обновлении пользователя.'});
+    }
+      return res.status(500).send({message: 'Произошла ошибка на сервере'});
+  });
 };
 
 module.exports.updateUserAvatar = (req, res) => {
@@ -55,12 +53,14 @@ module.exports.updateUserAvatar = (req, res) => {
   User.findByIdAndUpdate(req.user._id, {avatar}, {new: true, runValidators: true})
   .then((user) => {
     if (!user._id) {
-      throw new NotFoundError('Пользователь по указанному _id не найден.');
-    }
-    if (!user.avatar) {
-      throw new BadRequestError('Переданы некорректные данные при обновлении аватара.');
+      return res.status(404).send({message: 'Пользователь по указанному _id не найден.'})
     }
     res.status(200).send({data: user});
   })
-  .catch((err) => {throw new ServerError('Произошла ошибка на сервере')});
+  .catch((err) => {
+    if (err.name === 'ValidationError') {
+      return res.status(400).send({message: 'Переданы некорректные данные при обновлении пользователя.'});
+    }
+      return res.status(500).send({message: 'Произошла ошибка на сервере'});
+  });
 }
